facd969ee767460943be64e5b82c913b0784f186
Felix <felixfreidman@gmail.com>
2020-03-19 21:22:43 +0800

    Доделал сложную домашнюю работу

--- aviasales/scripts.js
+++ aviasales/scripts.js
@@ -1,79 +1,90 @@
-// Записываем сюда все элементы, с которыми работаем
-const formSearch = document.querySelector(".form-search"),
-  inputCitiesFrom = document.querySelector(".input__cities-from"),
-  dropdownCitiesFrom = document.querySelector(".dropdown__cities-from"),
-  inputCitiesTo = document.querySelector(".input__cities-to"),
-  dropdownCitiesTo = document.querySelector(".dropdown__cities-to"),
-  inputDateDepart = document.querySelector("input__date-depart");
-// Список городов и данные
+//получаем элементы со страницы
+const formSearch = document.querySelector('.form-search'),
+	inputCitiesFrom = formSearch.querySelector('.input__cities-from'),
+	dropdownCitiesFrom = formSearch.querySelector('.dropdown__cities-from'),
+	dropdownCitiesTo = formSearch.querySelector('.dropdown__cities-to'),
+	inputCitiesTo = formSearch.querySelector('.input__cities-to'),
+	inputDateDepart = formSearch.querySelector('.input__date-depart');
+
+//база городов, ключ АПИ и БД по календарю цен
+const CITY_API = 'http://api.travelpayouts.com/data/ru/cities.json',
+	CALENDAR = 'http://min-prices.aviasales.ru/calendar_preload',
+	PROXY = 'https://cors-anywhere.herokuapp.com/',
+	API_KEY = '5d2e3c92f5cbb5b20d19b432ed48fb95';
+/*'dataBase/cities.json' - та же БД городов, что и citiesApi, но локальная. 
+При вызове getData() вместо citiesApi (онлайн БД) убрать конкатенацию прокси со ссылкой*/
+
 let city = [];
-const citiesAPI = 'http://api.travelpayouts.com/data/ru/cities.json',
-  proxy = 'https://cors-anywhere.herokuapp.com/',
-  API_KEY = '2b6e5addd47843d5a38619e8982bc0ed',
-  calendar = 'http://min-prices.aviasales.ru/calendar_preload';
-// Функция для получения данных с сайта
+
+//функции, 2 секция
 const getData = (url, callback) => {
+	const request = new XMLHttpRequest();
 
-  const request = new XMLHttpRequest();
-  request.open('GET', url); //получаем данные 
-  request.addEventListener('readystatechange', () => {
-    if (request.readyState !== 4) return;
-    if (request.status === 200) {
-      callback(request.response); //проверяем, что получили все успешно
-    } else {
-      console.error(request.status); //выводим ошибку, если нет
-    }
-  });
+	request.open('GET', url);
 
-  request.send();
+	request.addEventListener('readystatechange', () => {
+		if (request.readyState !== 4) return;
 
-};
+		if (request.status === 200) {
+			callback(request.response);
+		} else {
+			console.error(request.status);
+		}
+	});
 
-// Функция, чтобы при клике на форму выводится список городов
-// Все сортируется в соответствии с буквами
-const showCity = (input, list) => {
-  list.textContent = "";
-  if (input.value !== "") {
-    const filterCity = city.filter(item => { //фильтруем сразу весь массив, лучше делать это тут, но внизу есть такой же фильтр
-      if (item.name) {
-        const fixItem = item.name.toLowerCase();
-        return fixItem.includes(input.value.toLowerCase());
-      }
-    });
-    filterCity.forEach(item => {
-      const li = document.createElement("li");
-      li.classList.add("dropdown__city");
-      li.textContent = item.name;
-      list.append(li);
-    });
-  }
-};
-const selectCity = (event, input, list) => { 
-  const target = event.target;
-  if (target.tagName.toUpperCase() === "LI") {
-    input.value = target.textContent;
-    list.textContent = "";
-  }
+	request.send();
 }
-// Вывод в поле откуда
-inputCitiesFrom.addEventListener("input", () => {
-  showCity(inputCitiesFrom, dropdownCitiesFrom);
+
+const showCity = (input, list) => {
+	list.textContent = '';
+	
+	if (input.value !== '') {
+		const filterCity = city.filter((item) => {
+			const fixItem = item.toLowerCase();
+			return fixItem.includes(input.value.toLowerCase());
+		});
+		
+		filterCity.forEach((item) => {
+			const li = document.createElement('li');
+			li.classList.add('dropdown__city');
+			li.textContent = item;
+			list.append(li);
+		});
+	} else {
+		return;
+	}
+};
+
+const handlerCity = (event, input, list) => {
+	const target = event.target;
+	if (target.tagName.toLowerCase() === 'li') {
+		input.value = target.textContent;
+		list.textContent = '';
+	}
+}
+
+//обработчики событий
+inputCitiesFrom.addEventListener('input', () => {
+	showCity(inputCitiesFrom, dropdownCitiesFrom)
 });
-// Выбор в поле куда
-inputCitiesTo.addEventListener("input", () => {
-  showCity(inputCitiesTo, dropdownCitiesTo);
+
+inputCitiesTo.addEventListener('input', () => {
+	showCity(inputCitiesTo, dropdownCitiesTo)
 });
-// Вывод в форме города, который мы выбрали
-dropdownCitiesFrom.addEventListener("click", (event) => {
-  selectCity(event, inputCitiesFrom, dropdownCitiesFrom);
+
+dropdownCitiesFrom.addEventListener('click', () => {
+	handlerCity(event, inputCitiesFrom, dropdownCitiesFrom);
 });
-dropdownCitiesTo.addEventListener("click", (event) => {
-  selectCity(event, inputCitiesTo, dropdownCitiesTo);
+
+dropdownCitiesTo.addEventListener('click', () => {
+	handlerCity(event, inputCitiesTo, dropdownCitiesTo);
 });
-getData(proxy + citiesAPI, (data) => {
-  city = JSON.parse(data).filter(item => item.name);
+
+//вызовы функций
+getData(PROXY + CITY_API, (data) => {
+	city = JSON.parse(data).filter(item => item.name);
 });
-// ДЗ: повторить все получить API TOKEN
-// Усложненное задание сделать один запрос 25 мая 
-// Екатеринбург Калининград через календарь
-// Показать в консоли
+
+//формирование и осуществление запроса на билет ЕКб - Калининград на 25 мая 2020 года
+const currentRequest = CALENDAR + `?origin=SVX&destination=KGD&depart_date=2020-05-25`;
+getData(currentRequest, data => console.log(JSON.parse(data)));
